3d8918a4686af4eabb156fbfddea7b4a5497c2e3
Nazanin Delam <nazanin1427@gmail.com>
2015-12-30 17:25:54 -0800

    DFS

--- graph.js
+++ graph.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 function Node(label) {
     this.label = label;
     this.adjacents = [];
@@ -7,9 +9,15 @@ Node.prototype.addAdjacent = function(node) {
     this.adjacents.push(node);
 }
 
-Node.prototype.addAdjacents = function(nodes) {
+Node.prototype.addAdjacents = function(nodes, created) {
     for(var adj in nodes) {
-        this.adjacents.push(nodes[adj]);
+        if(created[nodes[adj]]) {
+            this.adjacents.push(created[nodes[adj]]);
+        } else {
+            var newNode = new Node(nodes[adj]);
+            created[nodes[adj]] = newNode;
+            this.adjacents.push(newNode);
+        }
     }
 }
 
@@ -19,25 +27,56 @@ function Graph() {
 
 Graph.prototype.init = function(nodes) {
     var created = {};
+    //create
     for(var node in nodes) {
        for(var i in nodes[node]){
            if(!created[i]) {
                //create new node
                var newNode = new Node(i);
                //add its adjacents
-               newNode.addAdjacents(nodes[node][i]);
+               created[i] = newNode;
                this.nodes.push(newNode);
-               created[i] = [];
            }
        }
+    }
+
+    // Add adjacents
+    for(var node in nodes) {
+        for(var i in nodes[node]){
+            if(nodes[node][i].length > 0) {
+                _.each(nodes[node][i], function(adj) {
+                    created[i].addAdjacent(created[adj]);
+                });
+            }
+        }
+    }
+}
+
+Graph.prototype.DFS = function(start) {
+    var startNode, visited = {};
+    var stack = [];
+    //initialization
+     _.each(this.nodes, function(node) {
+        visited[node.label] = false;
+        if(node.label === start) {
+            startNode = node;
+            stack.push(startNode);
+        }
+    });
+    //get First node in stack
+    while(stack.length !== 0) {
+            var node = stack.pop();
+            console.log('****', node.label);
+            visited[node.label] = true;
+            _.each(node.adjacents, function(adj) {
+                if(!visited[adj.label]) {
+                    stack.push(adj);
+                }
+            });
     }
 }
 
 var g = new Graph();
-g.init([
-        {'A': ['B']},
-        {'B': ['C', 'A']},
-        {'C': ['A']}
-    ]);
+g.init([ {'A': ['B']}, {'B': ['C']}, {'C': ['A']}]);
 
-console.log(g.nodes);
+g.DFS('A');
