7805a05dd839688868d80bf2b7d1827f8d1b9e60
Nazanin Delam <nazanin1427@gmail.com>
2016-02-09 16:45:42 -0800

    Flatten an Array

--- src/test.js
+++ src/test.js
@@ -87,3 +87,48 @@ var JD = {};
 JD.debounce = function(func, wait, immediate) {
 
 }
+
+
+
+console.log(flatten([1,undefined,[2,[3]],[4]])); // => [1,undefined,2,3,4]
+
+//O(n)
+function flatten(arr) {
+
+  var result = [];
+  var toStringFn = Object.prototype.toString;
+  var nodes = arr && arr.slice();
+  var node;
+
+  if(!arr.length) {
+    return result;
+  }
+
+
+  while (nodes.length) {
+
+    node = nodes.pop();
+    if(toStringFn.call(node) === '[object Array]') {
+      nodes.push.apply(nodes, node);
+    } else {
+      result.push(node);
+    }
+
+  }
+
+  result.reverse();
+  return result;
+}
+// result = [1,undefined,3,2]  2,3,undefined,1
+
+
+
+// Recursive
+var backtrack = function (list, items) {
+    if(String.prototype.toString.call(list) !== '[object Array]') {
+        items.push(list);
+    }
+    for(var x in list) {
+        backtrack(list[x], items);
+    }
+}
