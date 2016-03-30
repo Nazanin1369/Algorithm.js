3bc84f9f35114049be17b822551728f8a8bb32b4
Nazanin Delam <nazanin1427@gmail.com>
2016-03-29 20:49:55 -0700

    test - negabinary

---
+++ src/test/negabinary.js
@@ -0,0 +1,22 @@
+/**
+ * Given a decimal number, write a function that returns its negabinary (i.e. negative 2-base) representation as a string.
+ * (-15) ==	'110001'
+ * (2) == '110'
+ * (13) == '11101'
+ */
+negabinary = num => {
+    if(isNaN(num))
+        return 'not a number!';
+
+    var nega = [],
+        remainder,
+        base = -2;
+    while(num !== 0) {
+        remainder = num % base;
+        num = Math.ceil(num / base);
+        nega.push(remainder > 0 ? remainder : -remainder)
+    }
+    return nega.reverse().join('');
+}
+
+console.log('-15: ', negabinary(-15), '  2: ', negabinary(2), '  13: ', negabinary(13));
