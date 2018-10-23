bd58ddb673198d58832789564c7537e5b3ca1e96
nazanin1369 <nazanin1427@gmail.com>
2018-10-22 22:44:23 -0700

    removed JS treeNode

--- src/treeNode.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var TreeNode = /** @class */ (function () {
-    function TreeNode(data) {
-        this.data = data;
-    }
-    return TreeNode;
-}());
-exports.TreeNode = TreeNode;
