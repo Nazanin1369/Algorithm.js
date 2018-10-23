cbb474b18b0f711f63db20cc7c6cf0a4a110b231
nazanin1369 <nazanin1427@gmail.com>
2018-10-23 00:14:05 -0700

    tire insert

--- src/trie.ts
+++ src/trie.ts
@@ -1,21 +1,40 @@
-
-export interface Map<T> {
-    [key: string]: T;
-}
-
 export class TrieNode {
     character: string;
     isEnd: boolean;
-    children: Map<TrieNode>
+    children: Map<string, TrieNode>
+
+    constructor(character?: string) {
+        this.character = character;
+        this.children = new Map<string, TrieNode>();
+    }
 }
 
 export class Trie {
 
-    map: Map<TrieNode>;
+    root: TrieNode;
+
+    constructor() {
+        this.root = new TrieNode();
+    }
 
     insert(word: string) {
-        for(let char of word.split('')) {
-            console.log(char)
+        let children = this.root.children,
+            level = 0;
+
+        for(let char of word) {
+            let node;
+            if(children.get(char)) {
+                node = children.get(char);
+            } else {
+                node = new TrieNode(char);
+                children.set(char, node);
+            }
+
+            children = node.children;
+
+            if (level++ === word.length - 1) {
+                node.isLeaf = true;
+            }
         }
     }
 
