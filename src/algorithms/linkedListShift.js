fff608862f5b39309906489048d67ddbdd5ff49d
Nazanin Delam <nazanin1427@gmail.com>
2018-12-14 23:07:20 -0800

    graph

--- src/algorithms/linkedListShift.js
+++ src/algorithms/linkedListShift.js
@@ -8,17 +8,8 @@ function rotate(list, times) {
         return list;
     }
 
-    let prevHead = list.head;
-    let rotationInfo = findRotationPoint(list, timesToRotate);
-    // Change the head to the new head
-    list.head = rotationInfo.newHead;
-    // Break the connection on the point
-    rotationInfo.penultimate.next = null;
-    // Move the first portion to the end
-    list.tail.next = prevHead;
-    list.tail = rotationInfo.penultimate;
-
-    return list;
+    // return rotateCounterClockwise(list , timesToRotate);
+    return rotateClockwise(list , timesToRotate);
 }
 
 function getRotationTimes(size, times) {
@@ -37,9 +28,36 @@ function findRotationPoint(list, timesToRotate) {
     return {newHead, penultimate};
 }
 
+function rotateCounterClockwise(list, timesToRotate) {
+    let prevHead = list.head;
+    let rotationInfo = findRotationPoint(list, timesToRotate);
+    // Change the head to the new head
+    list.head = rotationInfo.newHead;
+    // Break the connection on the point
+    rotationInfo.penultimate.next = null;
+    // Move the first portion to the end
+    list.tail.next = prevHead;
+    list.tail = rotationInfo.penultimate;
+
+    return list;
+}
+
+function rotateClockwise(list, timesToRotate) {
+    let prevHead = list.head;
+    let prevTail = list.tail;
+    let rotationInfo = findRotationPoint(list, list.length - timesToRotate);
+    console.log(rotationInfo)
+
+    rotationInfo.newHead.next = prevHead;
+    rotationInfo.penultimate.next = null;
+    list.head =  rotationInfo.newHead;
+    list.tail = rotationInfo.penultimate;
+    return list;
+}
+
 const list = createLinkedlist();
 const values = ['a', 'b', 'c', 'd', 'e'];
 const nodes = values.map(val => list.push(val));
 
 console.log(list.print())
-console.log(rotate(list, 21).print());
+console.log(rotate(list, 22).print());
--- src/datastructures/js/graph.js
+++ src/datastructures/js/graph.js
@@ -1,12 +1,82 @@
 'use struct';
 
-function createGraph() {
-    const neighbors = [];
+function createNode(key) {
+    const children = [];
+
     return {
         key,
-        neighbors,
-        addNeighbor(node) {
-            
+        children,
+        addChild(node) {
+            children.push(node);
         }
     }
 }
+
+function createGraph(directed = false) {
+    const nodes = [];
+    const edges = [];
+
+    return {
+        directed,
+        nodes,
+        edges,
+        addNode(key) {
+            nodes.push(createNode(key));
+        },
+
+        getNode(key) {
+            return nodes.find(n => n.key === key);
+        },
+
+        addEdge(node1Key, node2Key) {
+            const node1 = this.getNode(node1Key);
+            const node2 = this.getNode(node2Key);
+
+            node1.addChild(node2);
+
+            if(!directed){
+                node2.addChild(node1);
+            }
+
+            edges.push(`${node1Key}${node2Key}`);
+        },
+
+        print() {
+            return nodes
+            .map( ({children, key}) => {
+                let result = `${key}`;
+
+                if(children.length) {
+                    result += ` => ${
+                        children
+                        .map(node => node.key)
+                        .join(' ')}`
+                }
+
+                return result;
+            }).join('\n');
+        }
+    }
+}
+
+const graph = createGraph(true)
+
+graph.addNode('Kyle')
+graph.addNode('Anna')
+graph.addNode('Krios')
+graph.addNode('Tali')
+
+
+graph.addEdge('Kyle', 'Anna')
+graph.addEdge('Anna', 'Kyle')
+graph.addEdge('Kyle', 'Krios')
+graph.addEdge('Kyle', 'Tali')
+graph.addEdge('Anna', 'Krios')
+graph.addEdge('Anna', 'Tali')
+graph.addEdge('Krios', 'Anna')
+graph.addEdge('Tali', 'Kyle')
+
+console.log(graph.print())
+
+exports.createNode = createNode
+exports.createGraph = createGraph
