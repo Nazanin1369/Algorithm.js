eefbaf4d8341b6dc482dfc1a9fcc80c3f6092138
Nazanin Delam <nazanin1427@gmail.com>
2018-12-19 11:00:58 -0800

    Events

--- src/js/events/eventTarget.js
+++ src/js/events/eventTarget.js
@@ -1,26 +1,45 @@
 'use sttrict';
 
-var eventTarget = function() {
+var eventTarget = function () {
     this.listeners = {};
 };
 
 eventTarget.prototype.listeners = null;
 console.log(eventTarget.prototype);
 
-eventTarget.prototype.addEventListener = function(type, callback) {
-    if(!(type in this.listeners)) {
+eventTarget.prototype.addEventListener = function (type, callback) {
+    if (!(type in this.listeners)) {
         this.listeners[type] = [];
     }
 
     this.listeners[type].push(callback);
 }
 
-eventTarget.prototype.removeEventListener = function(type, callback) {
-    if(!(type in this.listeners)) {
+eventTarget.prototype.removeEventListener = function (type, callback) {
+    if (!(type in this.listeners)) {
         return;
     }
+
+    const stack = this.listeners[type];
+
+    for (let i = 0, l = stack.length; i < l; i++) {
+        if (stack[i] === callback) {
+            stack.splice(i, 1);
+            return;
+        }
+    }
+
+
 }
 
-eventTarget.prototype.dispatchEvent = function(event) {
+eventTarget.prototype.dispatchEvent = function (event) {
+    if (!(event.type in this.listeners)) {
+        return true;
+    }
+    var stack = this.listeners[event.type].slice();
 
+    for (var i = 0, l = stack.length; i < l; i++) {
+        stack[i].call(this, event);
+    }
+    return !event.defaultPrevented;
 }
